7733ff624eed4b3c60d51df0cb2d870b36cd62f7
emmabeynon <emma.beynon@gmail.com>
2015-12-08 09:13:37 +0000

    12: Created /named-cat route and edited @name variable to equal a name parameter

--- app.rb
+++ app.rb
@@ -16,7 +16,13 @@ get '/icecream' do
   "Yay ice cream"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
