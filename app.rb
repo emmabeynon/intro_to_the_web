f86a9eb89279751e0f8478c2aed444ad86a68b55
emmabeynon <emma.beynon@gmail.com>
2015-12-08 10:58:38 +0000

    14: Created /named-cat route to use post instead of get

--- app.rb
+++ app.rb
@@ -4,25 +4,17 @@ get '/' do
   "Hello!"
 end
 
-get '/secret' do
-  "It's a secret!"
-end
-
-get '/test' do
-  "It's a test!"
-end
-
-get '/icecream' do
-  "Yay ice cream"
-end
-
 get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb :cat_form
 end
